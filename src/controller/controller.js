3b49068f9d5bd7e086d2023d678ca6db528c73f5
Aji Muhammad Fauji <47517952+ajibon153@users.noreply.github.com>
2021-01-21 08:58:02 +0700

    finish

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,4 +1,7 @@
+const axios = require('axios');
 const API_KEY = '68a61494e620cbc977a4da9152fbb726';
+
+const Weather = require('../model/Weather');
 
 exports.renderHomePage = (req, res) => {
   res.render('index');
@@ -9,6 +12,26 @@ exports.renderAboutPage = (req, res) => {
 };
 
 exports.getWeather = (req, res) => {
-  console.log(req.body);
-  res.send('Your form has been submitted!');
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&appid=${API_KEY}&units=metric`;
+
+  const weather = new Weather(req.body.city);
+  weather.validateUserInput();
+
+  if (weather.error.length) {
+    res.render('index', {
+      error: weather.error.toString(),
+    });
+  } else {
+    axios
+      .get(url)
+      .then((response) => {
+        const { temp: temperture } = response.data.main;
+        const { name: location } = response.data;
+        res.render('index', {
+          weather: `It is currently ${temperture} in ${location}`,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+  // res.send('Your form has been submitted!');
 };
---
+++ src/model/Weather.js
@@ -0,0 +1,12 @@
+const Weather = function (data) {
+  this.data = data;
+  this.error = [];
+};
+
+Weather.prototype.validateUserInput = function () {
+  if (this.data === '') {
+    this.error.push('Please enter the name of the city');
+  }
+};
+
+module.exports = Weather;
